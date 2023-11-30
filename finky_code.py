d745c326ad7d9e3691f1a774e2e22db73dce3896
krichard07 <152333892+krichard07@users.noreply.github.com>
2023-11-30 10:02:35 +0100

    Add files via upload

---
+++ finky_code.py
@@ -0,0 +1,41 @@
+# 1. defining the character:
+
+char = "#"
+
+# 2. use the print function to display the letters and use f-strings to format the output
+
+# letter "C":
+
+print(f" {char *4}")
+print(f"{char} ")
+print(f"{char} ")
+print(f"{char} ")
+print(f" {char *4}")
+print("")
+
+# letter "O":
+
+print(f" {char *4}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f" {char *4}")
+print("")
+
+# letter "D":
+
+print(f"{char *3}")
+print(f"{char}   {char}")
+print(f"{char}    {char}")
+print(f"{char}   {char}")
+print(f"{char *3}")
+print("")
+
+# letter "E":
+
+print(f"{char *5}")
+print(f"{char}")
+print(f" {char *4}")
+print(f"{char}")
+print(f"{char *5}")
+print("")
